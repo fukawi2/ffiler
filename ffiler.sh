4fb9ccd2b5fd6f419e3b409e6cdcc8af93f3afad
Phillip Smith <phillip.smith@naturesorganics.com.au>
2016-04-08 10:13:44 +1000

    move file handling code into a function

--- ffiler.sh
+++ ffiler.sh
@@ -77,81 +77,27 @@ appendtrailingslash() {
   [[ $last_char != "/" ]] && str="$str/"
   echo "$str"
 }
-
-declare file_method=undefined
-declare file_depth=
-declare output_path=
-declare verbose=
-declare dry_run=
-declare action=m
-
-### fetch our cmdline options
-while getopts ":hs:d:o:MCLHvn" opt; do
-  case $opt in
-    s)  file_method=$OPTARG ;;
-    d)  file_depth=$OPTARG  ;;
-    o)  output_path="$(appendtrailingslash "$OPTARG")" ;;
-    M)  action=m            ;;  # action == move
-    C)  action=c            ;;  # action == copy
-    L)  action=l            ;;  # action == sym-link
-    H)  action=h            ;;  # action == hard-link
-    n)  dry_run=1
-        verbose=1           ;;
-    v)  verbose=1           ;;
-    h)  usage
-        exit 0              ;;
-    \?) echo "ERROR: Invalid option: -$OPTARG" >&2
-        usage
-        exit 1              ;;
-    :)  echo "ERROR: Option -$OPTARG requires an argument." >&2
-        exit 1              ;;
-    esac
-done
-shift $((OPTIND-1))
-
-# make these vars readonly to prevent accidentally changing them beyond this point
-readonly file_method file_depth output_path action dry_run verbose
-
-# validate user input
-[[ ! $file_method =~ ^[msSft]$ ]] && { bomb "Invalid filing method: $file_method"; }
-if [[ $file_method == 'm' ]] ; then
-  # depth is the timestamp granularity: ymdHMS
-  [[ ! $file_depth =~ ^[ymdHMS]$ ]] && { bomb "Invalid tree depth: $file_depth"; }
-elif [[ $file_method =~ ^[Ssf]$ ]] ; then
-  # depth is a number
-  [[ ! $file_depth =~ ^[0-9]+$ ]] && { bomb "Invalid tree depth: $file_depth"; }
-fi
-
-# validate $output_path
-if [[ -n "$output_path" ]] ; then
-  [[ -n $verbose ]] && printf 'Output path: %s\n' "$output_path"
-  [[ ! -d "$output_path" ]] && mkdir -p "$output_path"
-fi
-
-for X in "$@" ; do
-  fname="${1:-}"
-  shift
-
-  # validate the given user input
-  [[ -z "${fname}" ]]   && { usage; exit -1; }
-  # TODO: recurse into directories if given by user
-  #[[ -d "${fname}" ]] && recurse_this_directory_please
-  [[ ! -f "${fname}" ]] && { bomb "File not found: ${fname}"; }
+process_file() {
+  local _fname="$1"
+  local _file_method="$2"
+  local _file_depth="$3"
+  local _action="$4"
+  local _output_path="$5"
 
   # strip any leading path from the file name
-  base_fname=${fname##*/}
+  base_fname=${_fname##*/}
 
   # find the canonical path for the file
-  canon_fname=$(readlink --canonicalize "$fname")
+  canon_fname=$(readlink --canonicalize "$_fname")
 
   # get the file modified timestamp in epoch format
-  mod_tz_epoch=$(stat --format=%Y "$fname")
+  mod_tz_epoch=$(stat --format=%Y "$_fname")
 
   # work out the destination path for this file
   declare destdir=''
-  case $file_method in
+  case $_file_method in
     m)
-      case $file_depth in
+      case $_file_depth in
         y) destdir=$(date --date=@${mod_tz_epoch} +%Y/)                   ;;
         m) destdir=$(date --date=@${mod_tz_epoch} +%Y/%m-%b/)             ;;
         d) destdir=$(date --date=@${mod_tz_epoch} +%Y/%m-%b/%d/)          ;;
@@ -160,15 +106,15 @@ for X in "$@" ; do
         S) destdir=$(date --date=@${mod_tz_epoch} +%Y/%m-%b/%d/%H/%M/%S)  ;;
       esac ;;
     s) md5hash=$(printf '%s' "$base_fname" | md5sum | awk '{ print $1 }')
-       destdir=$(dirsplitfilename "$md5hash" $file_depth)      ;;
-    S) md5hash=$(md5sum "$fname" | awk '{ print $1 }')
-       destdir=$(dirsplitfilename "$md5hash" $file_depth)      ;;
-    f) destdir=$(dirsplitfilename "$base_fname" $file_depth)   ;;
-    t) destdir=$(file --brief --mime-type "$fname")           ;;
+       destdir=$(dirsplitfilename "$md5hash" $_file_depth)      ;;
+    S) md5hash=$(md5sum "$_fname" | awk '{ print $1 }')
+       destdir=$(dirsplitfilename "$md5hash" $_file_depth)      ;;
+    f) destdir=$(dirsplitfilename "$base_fname" $_file_depth)   ;;
+    t) destdir=$(file --brief --mime-type "$_fname")           ;;
   esac
 
   # prepend $output_path so we don't write to the cwd
-  destdir="$(printf '%s%s' "$output_path" "$destdir")"
+  destdir="$(printf '%s%s' "$_output_path" "$destdir")"
   if [[ ! -d "$destdir" ]] ; then
     [[ -n $verbose ]] && printf 'Create destination: %s\n' "$destdir"
     [[ -z $dry_run ]] && mkdir -p "$destdir"
@@ -176,23 +122,91 @@ for X in "$@" ; do
 
   # give feedback
   if [[ -n $verbose ]] ; then
-    case $action in
-      m) printf '[Move] %s  =>  %s\n' "$fname" "$destdir" ;;
-      c) printf '[Copy] %s  =>  %s\n' "$fname" "$destdir" ;;
-      l) printf '[Symlink] %s  =>  %s\n' "$fname" "$destdir" ;;
-      h) printf '[Hardlink] %s  =>  %s\n' "$fname" "$destdir" ;;
+    case $_action in
+      m) printf '[Move] %s  =>  %s\n' "$_fname" "$destdir" ;;
+      c) printf '[Copy] %s  =>  %s\n' "$_fname" "$destdir" ;;
+      l) printf '[Symlink] %s  =>  %s\n' "$_fname" "$destdir" ;;
+      h) printf '[Hardlink] %s  =>  %s\n' "$_fname" "$destdir" ;;
     esac
   fi
 
   # do the action
   if [[ -z $dry_run ]] ; then
-    case $action in
-      m) mv -f "$fname" "$destdir" ;;
-      c) cp -f "$fname" "$destdir" ;;
+    case $_action in
+      m) mv -f "$_fname" "$destdir" ;;
+      c) cp -f "$_fname" "$destdir" ;;
       l) ln -f --symbolic "$canon_fname" "$destdir" ;;
-      h) ln -f "$fname" "$destdir" ;;
+      h) ln -f "$_fname" "$destdir" ;;
   esac
   fi
-done
+}
 
+### Global Variables
+declare verbose=
+declare dry_run=
+
+### Main Code
+main() {
+  declare file_method=undefined
+  declare file_depth=
+  declare output_path=
+  declare action=m
+
+  ### fetch our cmdline options
+  while getopts ":hs:d:o:MCLHvn" opt; do
+    case $opt in
+      s)  file_method=$OPTARG ;;
+      d)  file_depth=$OPTARG  ;;
+      o)  output_path="$(appendtrailingslash "$OPTARG")" ;;
+      M)  action=m            ;;  # action == move
+      C)  action=c            ;;  # action == copy
+      L)  action=l            ;;  # action == sym-link
+      H)  action=h            ;;  # action == hard-link
+      n)  dry_run=1
+          verbose=1           ;;
+      v)  verbose=1           ;;
+      h)  usage
+          exit 0              ;;
+      \?) echo "ERROR: Invalid option: -$OPTARG" >&2
+          usage
+          exit 1              ;;
+      :)  echo "ERROR: Option -$OPTARG requires an argument." >&2
+          exit 1              ;;
+      esac
+  done
+  shift $((OPTIND-1))
+
+  # make these vars readonly to prevent accidentally changing them beyond this point
+  readonly file_method file_depth output_path action dry_run verbose
+
+  # validate user input
+  [[ ! $file_method =~ ^[msSft]$ ]] && { bomb "Invalid filing method: $file_method"; }
+  if [[ $file_method == 'm' ]] ; then
+    # depth is the timestamp granularity: ymdHMS
+    [[ ! $file_depth =~ ^[ymdHMS]$ ]] && { bomb "Invalid tree depth: $file_depth"; }
+  elif [[ $file_method =~ ^[Ssf]$ ]] ; then
+    # depth is a number
+    [[ ! $file_depth =~ ^[0-9]+$ ]] && { bomb "Invalid tree depth: $file_depth"; }
+  fi
+
+  # validate $output_path
+  if [[ -n "$output_path" ]] ; then
+    [[ -n $verbose ]] && printf 'Output path: %s\n' "$output_path"
+    [[ ! -d "$output_path" ]] && mkdir -p "$output_path"
+  fi
+
+  for X in "$@" ; do
+    fname="${1:-}"
+    shift
+
+    # validate the given user input
+    [[ -z "${fname}" ]]   && { usage; exit -1; }
+    #if [[ -d "${fname}" ]] && recurse_this_directory_please
+    [[ ! -f "${fname}" ]] && { bomb "File not found: ${fname}"; }
+
+    process_file "$fname" "$file_method" "$file_depth" "$action" "$output_path"
+  done
+}
+
+main $@
 exit 0
